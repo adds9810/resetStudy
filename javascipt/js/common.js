23eb6ed77e28ba17950bacd9b466701a740a34ab
jihye kim <46422329+adds9810@users.noreply.github.com>
2022-07-29 23:53:40 +0900

    자바스크립트 1주차 강의 필기

---
+++ javascipt/js/common.js
@@ -0,0 +1,11 @@
+/** 주석 방법 /** enter
+ * 함수에 대한 설명
+ * @param {*} productAmount 제품가격을 숫자로 전달
+ * @returns 부가세가 숫자현
+ */
+
+function taxAmount(productAmount) {
+  // 부가세 10% 가정
+  const tax = 0.1;
+  return prSoductAmount * tax;
+}
